2022266968dcbafd84fc232108ac4235547e764d
ohdmire <38505121+ohdmire@users.noreply.github.com>
2023-10-05 23:26:25 +0800

    v0.5

---
+++ nonebot_plugin_playercheck/__init__.py
@@ -0,0 +1,67 @@
+from nonebot import on_command,require
+from nonebot.adapters.onebot.v11 import GroupMessageEvent,Bot,Message,MessageSegment
+from nonebot.params import CommandArg
+from .datasource import getallgame,deleteplayer,addplayer
+import json
+import os
+from pathlib import Path
+
+
+require("nonebot_plugin_htmlrender")
+
+from nonebot_plugin_htmlrender import template_to_pic
+
+
+data_path=Path(".") / "data"
+template_path=str(Path(__file__).parent / "templates")
+template_name = "main.html"
+
+get_list = on_command("list",priority=5, block=True)
+# 获取所有用户的游戏列表
+@get_list.handle()
+async def getlist (event: GroupMessageEvent, bot: Bot):
+    if os.path.exists("data")==False:
+        os.mkdir("data")
+    if Path(str(data_path)+"/"+event.get_session_id().split("_")[1]+".json").exists()==False:
+        with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","w+",encoding="utf-8") as f:
+            json.dump([],f,ensure_ascii=False)
+    with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
+        jsondata=json.load(f)
+    allgamelists,playerdict,gamecoverdict=getallgame(jsondata,assertpath=str(data_path)+"/"+"assert.json")
+    usergroupdict={}
+    for i in playerdict.keys():
+        try:
+            useringroupname=await bot.get_group_member_info(group_id=event.get_session_id().split("_")[1],user_id=i)
+            usergroupname=useringroupname["card"]
+            if usergroupname == "":
+                usergroupname=useringroupname["nickname"]
+            usergroupdict[i]=usergroupname
+        except:
+            usergroupdict[i]="None"
+    templates={"allgamelists":allgamelists, "playerdict":playerdict,"usergroupdict" :usergroupdict, "gamecoverdict":gamecoverdict}
+    pages={ "viewport": { "width": 1600,"height": 1000 } } 
+    content = await template_to_pic(template_path=template_path,template_name=template_name,templates=templates,pages=pages)
+    await get_list.send(MessageSegment.image(content))
+
+delete_user = on_command("delete",priority=5, block=True)
+# 删除用户
+@delete_user.handle()
+async def deleteuser (event: GroupMessageEvent):
+    with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
+        jsondata=json.load(f)
+        deleteplayer(jsondata=jsondata,qq=event.get_user_id(),filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
+    await delete_user.finish("删除完毕")
+
+add_user = on_command("add",priority=5, block=True)
+# 添加用户
+@add_user.handle()
+async def adduser (event: GroupMessageEvent, args: Message = CommandArg()):
+    if os.path.exists("data")==False:
+        os.mkdir("data")
+    if Path(str(data_path)+"/"+event.get_session_id().split("_")[1]+".json").exists()==False:
+        with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","w+",encoding="utf-8") as f:
+            json.dump([],f,ensure_ascii=False)
+    with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
+        jsondata=json.load(f)
+        addplayer(jsondata=jsondata,qq=event.get_user_id(),gamelists=args.extract_plain_text().split(","),filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
+    await add_user.finish("添加完成")
---
+++ nonebot_plugin_playercheck/datasource.py
@@ -0,0 +1,70 @@
+import json
+# from flask import  Flask,render_template  #导入render_template模块
+# app=Flask(__name__)
+import operator
+
+# allgamelists=[]
+# playerdict={} #玩家字典，key是qq，value是游戏列表
+
+# 获取所有用户的游戏列表
+def getallgame(jsondata,assertpath):
+    def getgamename(gamename):
+        with open(assertpath,"r",encoding="utf-8") as f:
+            jsondata=json.load(f)
+        for i in jsondata:
+            if gamename in i["alias"]:
+                return i["name"]
+        return gamename
+    
+    def getpicurl(gamename):
+        with open(assertpath,"r",encoding="utf-8") as f:
+            jsondata=json.load(f)
+        for i in jsondata:
+            if gamename in i["name"]:
+                return i["url"]
+            pass
+
+    allgamelists=[]
+    playerdict={} #玩家字典，key是qq，value是游戏列表
+    gamecoverdict={} #游戏封面字典，key是游戏名，value是封面url
+    for i in jsondata: #每一个i是一个用户
+        allgame=i["gamelists"]
+        nametemplist=[]
+        for j in allgame:
+            j=getgamename(j)
+            nametemplist.append(j) #替换别名
+            playerdict[i["qq"]]=nametemplist
+            if j not in allgamelists:
+                allgamelists.append(j)
+    
+    for i in allgamelists: #获取游戏封面
+        gamecoverdict[i]=getpicurl(i)
+
+    allgamelists.sort()
+    sort_key_dic_instance = dict(sorted(playerdict.items(), key=operator.itemgetter(0)))  #按照key值升序
+
+
+    return allgamelists,sort_key_dic_instance,gamecoverdict
+
+# 删除用户
+def deleteplayer(jsondata,qq,filepath):
+    newjsondata=[]
+    for i in jsondata:
+        if i["qq"]==qq:
+            pass
+        else:
+            newjsondata.append(i)
+    with open(filepath,"w",encoding="utf-8") as f:
+        json.dump(newjsondata,f,ensure_ascii=False)
+
+# 添加用户和游戏列表
+def addplayer(jsondata,qq,gamelists,filepath):
+    newjsondata=[]
+    for i in jsondata:
+        if i["qq"]==qq:
+            pass
+        else:
+            newjsondata.append(i)
+    newjsondata.append({"qq":qq,"gamelists":gamelists})
+    with open(filepath,"w",encoding="utf-8") as f:
+        json.dump(newjsondata, f,ensure_ascii=False)
