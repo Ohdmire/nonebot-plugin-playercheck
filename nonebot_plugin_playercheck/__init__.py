cdb0893d8f712c465be51eeba6ec34a37d51e560
ohdmire <38505121+ohdmire@users.noreply.github.com>
2023-10-08 15:39:07 +0800

    v1.5.0
    - 修复了删除时乱删的bug (#3)
    
    - 优化了代码，重写了json读写相关代码
    
    - 优化assets.json支持多个别名指向一个名字
    
    - 还修复了一些奇怪的群成员名获取的bug

--- nonebot_plugin_playercheck/__init__.py
+++ nonebot_plugin_playercheck/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command,require
 from nonebot.adapters.onebot.v11 import GroupMessageEvent,Bot,Message,MessageSegment
 from nonebot.params import CommandArg
 from nonebot.plugin import PluginMetadata
-from .datasource import getallgame,deleteplayer,addplayer
+from .datasource import get_all_info,deleteplayer,addplayer
 
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-playercheck",
@@ -19,6 +19,7 @@ __plugin_meta__ = PluginMetadata(
 
 import json
 from pathlib import Path
+import operator
 
 require("nonebot_plugin_htmlrender")
 from nonebot_plugin_htmlrender import template_to_pic
@@ -27,50 +28,63 @@ data_path=Path(".") / "data" / "playercheck"
 template_path=str(Path(__file__).parent / "templates")
 template_name = "main.html"
 
-get_list = on_command("cf.list",priority=5, block=True)
 # 获取所有用户的游戏列表
+get_list = on_command("cf.list",priority=5, block=True)
 @get_list.handle()
 async def getlist (event: GroupMessageEvent, bot: Bot):
+    #读取数据部分
     data_path.mkdir(parents=True, exist_ok=True)
     if Path(str(data_path)+"/"+event.get_session_id().split("_")[1]+".json").exists()==False:
         with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","w+",encoding="utf-8") as f:
             json.dump([],f,ensure_ascii=False)
     with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
         jsondata=json.load(f)
-    allgamelists,playerdict,gamecoverdict=getallgame(jsondata,assetspath=str(data_path)+"/"+"assets.json")
-    usergroupdict={}
-    for i in playerdict.keys():
-        try:
-            useringroupname=await bot.get_group_member_info(group_id=event.get_session_id().split("_")[1],user_id=i)
-            usergroupname=useringroupname["card"]
-            if usergroupname == "":
-                usergroupname=useringroupname["nickname"]
-            usergroupdict[i]=usergroupname
-        except:
-            usergroupdict[i]="None"
-    templates={"allgamelists":allgamelists, "playerdict":playerdict,"usergroupdict" :usergroupdict, "gamecoverdict":gamecoverdict}
+
+    allgamelists,gamename2aliasdict,gamename2urldict=get_all_info(jsondata,assetspath=str(data_path)+"/"+"assets.json")
+
+    #这里处理一下群友列表，把qq号和群名片对应起来，没有就用昵称
+    groupmemberlists=await bot.get_group_member_list(group_id=event.get_session_id().split("_")[1])
+    user2cardname={}
+    for i in groupmemberlists: #i是一个字典，包含qq，群名片，昵称等信息
+        userqq=i["user_id"]
+        usercardname=i["card"]
+        if usercardname == "":
+            usercardname=i["nickname"]
+        user2cardname.update({str(userqq):str(usercardname)})
+
+    #按照群名片排序
+    user2cardname = dict(sorted(user2cardname.items(), key=operator.itemgetter(1)))
+
+    #playerdict就是jsondata
+    templates={"allgamelists":allgamelists, "jsondata":jsondata,"user2cardname" :user2cardname, "gamename2urldict":gamename2urldict}
     pages={ "viewport": { "width": 1600,"height": 1000 } } 
     content = await template_to_pic(template_path=template_path,template_name=template_name,templates=templates,pages=pages)
     await get_list.send(MessageSegment.image(content))
 
-delete_user = on_command("cf.del",priority=5, block=True)
 # 删除用户
+delete_user = on_command("cf.del",priority=5, block=True)
 @delete_user.handle()
 async def deleteuser (event: GroupMessageEvent):
     with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
         jsondata=json.load(f)
-        deleteplayer(jsondata=jsondata,qq=event.get_user_id(),filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
+    deleteplayer(jsondata=jsondata,qq=event.get_user_id(),filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
     await delete_user.finish("删除完毕")
 
-add_user = on_command("cf.add",priority=5, block=True)
 # 添加用户
+add_user = on_command("cf.add",priority=5, block=True)
 @add_user.handle()
 async def adduser (event: GroupMessageEvent, args: Message = CommandArg()):
+    #这里多了个判断，第一次用应该没有创建群号.json，所以要创建
     data_path.mkdir(parents=True, exist_ok=True)
     if Path(str(data_path)+"/"+event.get_session_id().split("_")[1]+".json").exists()==False:
         with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","w+",encoding="utf-8") as f:
             json.dump([],f,ensure_ascii=False)
+    #这里和删除用户的部分一样
     with open (str(data_path)+"/"+event.get_session_id().split("_")[1]+".json","r",encoding="utf-8") as f:
         jsondata=json.load(f)
-        addplayer(jsondata=jsondata,qq=event.get_user_id(),gamelists=args.extract_plain_text().split(","),filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
+    gamename2aliasdict=get_all_info(jsondata,assetspath=str(data_path)+"/"+"assets.json")[1]
+    addplayer(jsondata=jsondata,qq=event.get_user_id(),gamelists=args.extract_plain_text().split(","),gamename2aliasdict=gamename2aliasdict,filepath=str(data_path)+"/"+event.get_session_id().split("_")[1]+".json")
+
     await add_user.finish("添加完成")
+
+#TO DO私聊部分
--- nonebot_plugin_playercheck/datasource.py
+++ nonebot_plugin_playercheck/datasource.py
@@ -1,73 +1,84 @@
 import json
 import os
-import operator
-
 
 # 获取所有用户的游戏列表
-def getallgame(jsondata,assetspath):
-    def getgamename(gamename):
-        if os.path.exists(assetspath)==True:
-            with open(assetspath,"r",encoding="utf-8") as f:
-                jsondata=json.load(f)
-            for i in jsondata:
-                if gamename in i["alias"]:
-                    return i["name"]
-            return gamename
-        else:
-            return gamename
+def get_all_info(jsondata,assetspath):
+    if os.path.exists(assetspath)==True:    
+        with open(assetspath,"r",encoding="utf-8") as f:
+            assetsdata=json.load(f)
+    else:
+        assetsdata=[]
+        
+    #对于json来说，三个变量相互关联的话，我必须至少创建出两个字典，确保数据的一致性
+    gamename2aliasdict={} #游戏名与别名的对应字典
+    gamename2urldict={} #游戏名与url的对应字典
     
-    def getpicurl(gamename):
-        if os.path.exists(assetspath)==True:
-            with open(assetspath,"r",encoding="utf-8") as f:
-                jsondata=json.load(f)
-            for i in jsondata:
-                if gamename in i["name"]:
-                    return i["url"]
-                pass
-        else:
-            pass
+    #读取assets.json，获取别名与url的对应字典
+    for i in assetsdata: #一个i是一个字典{"name":"osu","alias":["osu","osu!"],"url":"https://osu.ppy.sh"}
+        gamename2aliasdict.update({i["name"]:i["alias"]})
+        gamename2urldict.update({i["name"]:i["url"]})
 
+    #获取储存的玩家游戏列表
+    gamenametemplist=[]
+    for i in jsondata: #一个i是一个字典{"114514":["osu","malody"]}
+        for value in i.values():
+            gamenametemplist.extend(value)
+            gamenametemplist=list(set(gamenametemplist)) #去重
+    
+    #替换别名,没有就是输入的alias当作游戏名
     allgamelists=[]
-    playerdict={} #玩家字典，key是qq，value是游戏列表
-    gamecoverdict={} #游戏封面字典，key是游戏名，value是封面url
-    for i in jsondata: #每一个i是一个用户
-        allgame=i["gamelists"]
-        nametemplist=[]
-        for j in allgame:
-            j=getgamename(j)
-            nametemplist.append(j) #替换别名
-            playerdict[i["qq"]]=nametemplist
-            if j not in allgamelists:
-                allgamelists.append(j)
-    
-    for i in allgamelists: #获取游戏封面
-        gamecoverdict[i]=getpicurl(i)
+    for i in gamenametemplist: #一个i是一个alias列表的一个值
+        if gamename2aliasdict=={}:
+            allgamelists.append(i)
+            continue
+        for key,value in gamename2aliasdict.items(): #key是一个游戏名，一个value是一个别名列表(alias)
+            if i in value: #如果i在values内,就把key加入allgamelists
+                allgamelists.append(key)
+            else:
+                allgamelists.append(i) #如果i不在values内，就把i加入allgamelists
+    allgamelists=list(set(allgamelists)) #去重
+    allgamelists.sort() #排序
 
-    allgamelists.sort()
-    sort_key_dic_instance = dict(sorted(playerdict.items(), key=operator.itemgetter(0)))  #按照key值升序
-
-
-    return allgamelists,sort_key_dic_instance,gamecoverdict
+    return allgamelists,gamename2aliasdict,gamename2urldict
+ 
 
 # 删除用户
 def deleteplayer(jsondata,qq,filepath):
     newjsondata=[]
     for i in jsondata:
-        if i["qq"]==qq:
-            pass
-        else:
-            newjsondata.append(i)
+        for k in i.keys(): #k是字典,key是qq号,value是游戏列表
+            if k != qq:
+                newjsondata.append(i)
     with open(filepath,"w",encoding="utf-8") as f:
-        json.dump(newjsondata,f,ensure_ascii=False)
+        json.dump(newjsondata, f,ensure_ascii=False)
 
 # 添加用户和游戏列表
-def addplayer(jsondata,qq,gamelists,filepath):
+def addplayer(jsondata,qq,gamelists,filepath,gamename2aliasdict):
+    #替换别名,没有就是输入的alias当作游戏名
+    newgamelists=[]
+    for i in gamelists: #一个i是一个alias列表的一个值
+        if gamename2aliasdict=={}:
+            newgamelists.append(i)
+            continue
+        for key,value in gamename2aliasdict.items(): #key是一个游戏名，一个value是一个别名列表(alias)
+            if i in value:
+                newgamelists.append(key)
+                break
+            else:
+                newgamelists.append(i)
+                break
+
+    newgamelists=list(set(newgamelists)) #去重
+
+    #修改jsondata然后写入文件
     newjsondata=[]
-    for i in jsondata:
-        if i["qq"]==qq:
-            pass
-        else:
+    if jsondata == []:
+        newjsondata.append({qq:newgamelists})
+    else:
+        for i in jsondata: 
+            if qq in i.keys():
+                i.update({qq:newgamelists})
             newjsondata.append(i)
-    newjsondata.append({"qq":qq,"gamelists":gamelists})
+
     with open(filepath,"w",encoding="utf-8") as f:
         json.dump(newjsondata, f,ensure_ascii=False)
